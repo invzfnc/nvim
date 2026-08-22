fdd845e11937ecd6940a83c3de0135b4a8c9f2db
cha <t1110020m@gmail.com>
2026-08-23 03:27:00 +0800

    switch to windows terminal, fix transparency issue

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,8 +1,13 @@
 return {
   {
     "folke/tokyonight.nvim",
+    opts = {
+      style = "moon",
+      transparent = true,
+    },
     config = function()
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd.colorscheme "tokyonight"
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     end
   },
 }
