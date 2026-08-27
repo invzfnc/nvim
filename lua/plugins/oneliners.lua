06f0b3feb69bc57ca8453d6c409123c1feac70ef
cha <t1110020m@gmail.com>
2026-08-27 13:04:34 +0800

    lazy load todo-comments and gitgutter

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -6,10 +6,12 @@ return {
   { -- style todo comments, and others like "warn", "bug"
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    event = { "BufReadPost", "BufNewFile" },
     opts = { signs = false },
   },
 
   { -- show git diff - added, modified, removed
     "airblade/vim-gitgutter",
+    event = { "BufReadPost", "BufNewFile" },
   }
 }
