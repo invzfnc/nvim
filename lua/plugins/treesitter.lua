96c80c9e36ecc8b2c334998d720b82f8032972d0
cha <t1110020m@gmail.com>
2026-08-27 12:08:58 +0800

    fix and clean treesitter configs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,84 +1,22 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-        local ts = require("nvim-treesitter")
+    build = ":TSUpdate",  -- auto update parsers on plugin update
+    event = { "BufReadPost", "BufNewFile" },  -- lazy loads when opening a file
 
-        ts.setup({
-            ensure_installed = {
-                "lua", "python", "markdown", "vim", "javascript", 
-                "html", "css", "typescript", "tsx", "json", "java",
-                "c", "cpp"
-            },
-            auto_install = false,
-            sync_install = false,
-        })
+    opts = {
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
 
-        vim.api.nvim_create_autocmd("FileType", {
-            callback = function()
-                if pcall(vim.treesitter.start) then
-                    vim.treesitter.start()
-                end
-            end,
-        })
-    end
+        ensure_installed = {
+            "lua", "vim", "c", "cpp", "python", "markdown", "javascript",
+        },
+
+        sync_install = false,  -- do async install, don't freeze neovim on startup
+        auto_install = true,
+
+    },
 }
 
---[[
-return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-        local ts = require("nvim-treesitter")
-        local languages = {
-            "lua", "python", "markdown", "vim", "javascript", 
-            "html", "css", "typescript", "tsx", "json", "java",
-            "c", "cpp"
-        }
-        ts.setup({})
-
-        -- Use TSInstall to manually install languages
-        ts.install(languages)
-
-        -- Treesitter features for installed languages must be enabled manually
-        vim.api.nvim.create_autocmd("FileType", {
-            pattern = languages,
-            callback = function()
-                -- Enable native neovim treesitter highlighting
-                vim.treesitter.start()
-
-                -- Configure code folding
-                vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-                vim.wo.foldmethod = "expr"
-                vim.foldlevel = 99
-
-                -- Enable treesitter-based indentation
-                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-            end,
-        })
-    end,
-}
-]]--
-
---[[
-return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            highlight = { enable = true, },
-            indent = { enable = true, },
-            autotage = { enable = true, },
-            ensure_installed = {
-                "lua", "python", "markdown", "vim", "javascript", 
-                "html", "css", "typescript", "tsx", "json", "java",
-                "c", "cpp"
-            },
-            auto_install = false,
-        })
-    end
-}
-]]--
