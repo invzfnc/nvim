7a4cd99b8cf5bf376c3e2778a4c98253fe4b1e25
cha <t1110020m@gmail.com>
2026-08-27 11:17:41 +0800

    cleanup files

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -7,7 +7,7 @@ return {
     },
     config = function()
       vim.cmd.colorscheme "tokyonight"
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- add transparency
     end
   },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,7 +11,6 @@ return {
         vim.keymap.set('n', "<leader>fg", builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', "<leader>fb", builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', "<leader>fh", builtin.help_tags, { desc = 'Telescope help tags' })
-        --vim.keymap.set('n', "<leader>en", builtin.find_files, { cwd = vim.fn.stdpath("config") })
         vim.keymap.set("n", "<leader>en", function()
             builtin.find_files {
                 cwd = vim.fn.stdpath("config")
@@ -20,6 +19,6 @@ return {
     end
 }
 
--- extra dependencies:
+-- extra dependencies to install:
 -- winget install BurntSushi.ripgrep.MSVC
 -- winget install sharkdp.fd 
