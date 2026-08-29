3d3936741b5040b76c3d6b332a2ec0a92f9afb9d
cha <t1110020m@gmail.com>
2026-08-29 15:28:07 +0800

    set retrobox theme

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -20,3 +20,21 @@ vim.keymap.set("n", "<C-c>", "<cmd>nohlsearch<CR>")
 -- don't show the mode, it's already in the status line
 vim.o.showmode = false
 
+-- colorscheme and transparency
+vim.cmd.colorscheme("retrobox")
+--vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- add transparency
+
+local transparent_groups = {
+  "Normal",
+  "NormalFloat",
+  "SignColumn",
+  "LineNr",
+  "CursorLineNr",
+  "Folded",
+  "NonText",
+  "SpecialKey",
+}
+
+for _, group in ipairs(transparent_groups) do
+  vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+end
